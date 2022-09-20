591f37aace7222df5049d88d4ea5f2cc268ebb54
Jonas Kirk Pedersen <jonas_pedersen@hotmail.com>
2022-09-20 23:10:34 +0200

    Change ip

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "azurerm_network_security_rule" "mtc-dev-rule" {
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefix       = "185.107.14.108/32"
+  source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.mtc-rg.name
   network_security_group_name = azurerm_network_security_group.mtc-sg.name
